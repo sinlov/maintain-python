9ac57456a404516d400306d9c096c267fb872146
sinlov <sinlovgmppt@gmail.com>
2021-01-06 18:06:34 +0800

    fix: fix gradle-depend.sh error of use

--- language/gradle/gradle-depend.sh
+++ language/gradle/gradle-depend.sh
@@ -4,6 +4,22 @@ run_path=$(pwd)
 shell_run_name=$(basename $0)
 shell_run_path=$(cd `dirname $0`; pwd)
 
+pV(){
+    echo -e "\033[;36m$1\033[0m"
+}
+pI(){
+    echo -e "\033[;32mINFO: $1\033[0m"
+}
+pD(){
+    echo -e "\033[;34mDEBUG: $1\033[0m"
+}
+pW(){
+    echo -e "\033[;33mWARN: $1\033[0m"
+}
+pE(){
+    echo -e "\033[;31mERROR: $1\033[0m"
+}
+
 checkFuncBack(){
   if [ $? -ne 0 ]; then
     echo -e "\033[;31mRun [ $1 ] error exit code 1\033[0m"
@@ -13,15 +29,15 @@ checkFuncBack(){
 fi
 }
 
-checkEnv(){
-  evn_checker=`which $1`
-  checkFuncBack "which $1"
-  if [ ! -n "evn_checker" ]; then
-    echo -e "\033[;31mCheck event [ $1 ] error exit\033[0m"
-    exit 1
-  # else
-  #   echo -e "\033[;32mCli [ $1 ] event check success\033[0m\n-> \033[;34m$1 at Path: ${evn_checker}\033[0m"
-fi
+checkBinary(){
+    binary_checker=`which $1`
+    checkFuncBack "which $1"
+    if [[ ! -n "${binary_checker}" ]]; then
+        echo -e "\033[;31mCheck binary [ $1 ] error exit\033[0m"
+        exit 1
+        #  else
+        #    echo -e "\033[;32mCli [ $1 ] binary check success\033[0m\n-> \033[;34m$1 at Path: ${binary_checker}\033[0m"
+    fi
 }
 
 checkRunPathGradlew(){
@@ -61,42 +77,64 @@ runGradleDependenciesTask(){
     ${run_path}/gradlew -q "$1dependencies" --configuration "$2"
 }
 
-# checkEnv
-checkEnv java
-checkEnv android
-checkEnv gradle
+# checkBinary
+checkBinary java
+checkBinary android
+checkBinary gradle
 checkRunPathGradlew
 
 depend_conf_mode="implementation"
 
-help_info="This gradle-depend.sh can fast see gradle project depends\n
-\t-h see help\n
-\n
-Fast see mode ${depend_conf_mode}\n
-\t You can use \033[;34m gradle-depend.sh [moduleName]\033[0m\n
-\n
-If want other mode can use as\n
-\t-m module set \033[;32mIf set -m script will check module exist! \033[0m\n
-\t-c compile mode \033[;36m gradle-depend.sh -m [moduleName] -c\033[0m\n
-\t-r runtime mode \033[;36m gradle-depend.sh -m [moduleName] -r\033[0m\n
-\t-f full mode \033[;36m gradle-depend.sh -m [moduleName] -f\033[0m\n
-\t-t task show \033[;36m gradle-depend.sh -m [moduleName] -t\033[0m\n
-"
+function Usage(){
+cat << HELP
+Usage: ${shell_run_name} [moduleName]
+
+Example:
+  ${shell_run_name} app
+
+Help:
+  This ${shell_run_name} can fast see gradle project depends
+
+  -m module set If set -m script will check module exist!
+  -t task             show  ${shell_run_name} -m [moduleName] -t
+  -b buildEnvironment mode  ${shell_run_name} -m [moduleName] -b
+  -c compile          mode  ${shell_run_name} -m [moduleName] -c
+  -r releaseCompile   mode  ${shell_run_name} -m [moduleName] -r
+  -f full             mode  ${shell_run_name} -m [moduleName] -f
+HELP
+}
+
+ARG=$1
+if [[ "$ARG" =~ "-h" ]];then
+    Usage
+    exit 0
+fi
 
 if [ $# == 0 ]; then
     echo "unknown params, please see help -h"
     exit 1
 elif [ $# == 1 ]; then
-    if [ $1 == "-h" ]; then
-       echo -e ${help_info}
-    else
-       reduceGradleModule $1
-       runGradleDependenciesTask ${android_build_module} ${depend_conf_mode}
-    fi
+    case $1 in
+        "-b" )
+            pI "=> buildEnvironment mode"
+            ${run_path}/gradlew buildEnvironment
+            exit 0
+        ;;
+        "-t" )
+            pI "=> task mode"
+            ${run_path}/gradlew task
+            exit 0
+        ;;
+        * )
+            pI "=> fast show module dependencies mode"
+            reduceGradleModule $1
+            runGradleDependenciesTask ${android_build_module} ${depend_conf_mode}
+        ;;
+        esac
 else
     run_gradle_module=""
     other_module=""
-    while getopts "hm:crft" arg #after param has ":" need option
+    while getopts "hm:bcrft" arg #after param has ":" need option
     do
         case $arg in
             h)
@@ -107,16 +145,24 @@ else
                 reduceGradleModule ${OPTARG}
                 run_gradle_module=${android_build_module}
             ;;
+            b)
+                ${run_path}/gradlew buildEnvironment
+                exit 0
+            ;;
             c)
+                pI "=> compile mode"
                 depend_conf_mode="compile"
             ;;
             r)
+                pI "=> releaseCompile mode"
                 depend_conf_mode="releaseCompileClasspath"
             ;;
             f)
+                pI "=> full mode at ${run_gradle_module}"
                 other_module="dependencies"
             ;;
             t)
+                pI "=> task show at ${run_gradle_module}"
                 other_module="tasks"
             ;;
         esac
